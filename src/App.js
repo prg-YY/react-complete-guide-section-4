c50ab4e9cb1754a054a9771e19141a4f4366c117
prg0615 <parguzatt@gmail.com>
2021-12-22 19:06:24 +0100

    Section-4-Udemey-React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Expenses from "./components/Expenses/Expenses"
+import NewExpense from "./components/NewExpense/NewExpense"
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    {
+      id: "e2",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e3",
+      title: "house rent",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+    {
+      id: "e4",
+      title: "Restuarant fee",
+      amount: 329,
+      date: new Date(2021, 3, 13),
+    },
+  ]
 
-function App() {
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js")
+    console.log(expense)
+  }
+
+  // return React.createElement(
+  //   "div",
+  //   {},
+  //   React.createElement("h2", {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
